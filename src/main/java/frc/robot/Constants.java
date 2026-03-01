2aec05918f8cdb31275deaf797a4d862fd264f86
Zane Jacobi <zane05jacobi@gmail.com>
2026-02-28 19:21:28 -0600

    Commit before moving away from turret

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
@@ -22,13 +21,12 @@ public final class Constants {
     public static final int DRIVE_CONTROLLER_PORT = 0;
     public static final int OPERATOR_CONTROLLER_PORT = 1;
 
-    public static final int NEO_ENCODER_COUNTS_PER_REVOLUTION = 42;
-
     public static final int INTAKE_CAN_ID = 15;
     public static final double INTAKE_SPEED = 0.1;
     public static final int INTAKE_CURRENT_LIMIT = 20;
 
     public static final int INTAKE_ARM_CAN_ID = 14;
+    // Should probably be 110, set low for testing
     public static final Angle INTAKE_ARM_EXTENSION_ANGLE = Units.Degrees.of(50);
     public static final double INTAKE_ARM_GEAR_RATIO = 20;
     public static final int INTAKE_ARM_CURRENT_LIMIT = 20;
